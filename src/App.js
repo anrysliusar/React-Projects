e3108bbccff9f8b6be13579d9ddda59f551e6dcb
andri <assliusarenko@gmail.com>
2020-12-05 16:32:02 +0200

    update files

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
 import React, {Component} from 'react';
-import {cars} from "./dataBase/cars";
-import CarComponent from "./components/CarComponent"
-import {users} from "./dataBase/users";
-import UserComponent from "./components/UserComponent";
-
+import AllPosts from "./components/AllPosts";
 
 class App extends Component {
     render() {
         return (
             <div>
-                <div><h2>Автомобілі</h2>
-                    <hr/>
-
-                    {
-                        cars.map((car, index) =>{
-                            return <CarComponent
-                                item = {car}
-                                key = {index}/>
-                        })
-                    }
-                    <hr/>
-                </div>
-                <div><h2>Користувачі</h2>
-                    <hr/>
-                    {
-                        users.map((user, index) =>{
-                            return <UserComponent
-                                item = {user}
-                                key = {index}/>
-                        })
-                    }
-                    <hr/>
-                </div>
+                {
+                    <AllPosts/>
+                }
             </div>
-
         );
     }
 }
