6f628a9f63cd92a2390bd7ef3de91174aa279491
andri <assliusarenko@gmail.com>
2020-12-05 16:30:56 +0200

    add PostComponent

---
+++ src/components/PostComponent.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import "./stylePost.css"
+
+class PostComponent extends Component {
+    render() {
+        let {item} = this.props
+        return (
+            <div>
+                <h3 className={'post-title'}>{item.id}-{item.title}</h3>
+                <p className={'post-body'}>{item.body}</p>
+            </div>
+        );
+    }
+}
+
+export default PostComponent;
