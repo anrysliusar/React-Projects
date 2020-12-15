d12b9116c9edeadfae581a0c4f760762010fd1d3
freefireman <beluh.c.s@gmail.com>
2020-12-15 20:55:22 +0200

    little fix)))

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,10 @@
 export default class UserServices {
     url = 'https://jsonplaceholder.typicode.com'
 
+    findUserById(id){
+        return fetch(`${this.url}/users/${id}`)
+            .then(value => value.json())
+    }
 
     async users() {
         return fetch(`${this.url}/users`)
